331873df00f14f97caa737013ba65ca6ff9389ba
Tyler Dewey <ty.dewey@gmail.com>
2014-10-08 10:18:43 -0500

    Code cleanup and documentation

--- CityMerger.java
+++ CityMerger.java
@@ -3,7 +3,6 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -18,12 +17,13 @@ public class CityMerger {
 			"north-dakota", "south-dakota", "wisconsin");
 
 	/**
+	 * Retrieves a list of scanners for a given set of states' .cities files
 	 * 
 	 * @param states
-	 *            an array of state names
+	 *            an array of valid state names
 	 * @return an array of Scanners of the appropriate state files
 	 */
-	protected static List<Scanner> getScanners(List<String> states)
+	protected static List<Scanner> scannersForStates(List<String> states)
 			throws FileNotFoundException {
 		String path;
 		FileReader file;
@@ -42,22 +42,26 @@ public class CityMerger {
 	} // getScanners(List<String>)
 
 	/**
+	 * merge sorted file input into a given output stream
 	 * 
-	 * @param arrays
-	 *            the sorted array to merge
-	 * @return a sorted array of the values from arrays
+	 * @param scanners
+	 *            the scanners that point to the sorted files
+	 * @param out
+	 *            the PrintWriter with which output will be written
+	 * @throws NoSuchElementException
+	 * @throws IllegalStateException
 	 */
-	static void mergeCities(List<Scanner> stateScanners, PrintWriter out)
+	static void sortedFileMerge(List<Scanner> scanners, PrintWriter out)
 			throws NoSuchElementException, IllegalStateException {
 		Scanner scanner;
 		PQItem item;
 
 		// create the queue
-		PriorityQueue queue = new PriorityQueue(stateScanners.size());
+		PriorityQueue queue = new PriorityQueue(scanners.size());
 
 		// fill the queue with initial values
-		for (int i = 0; i < stateScanners.size(); i++) {
-			scanner = stateScanners.get(i);
+		for (int i = 0; i < scanners.size(); i++) {
+			scanner = scanners.get(i);
 
 			if (scanner.hasNextLine()) {
 				queue.insert(new PQItem(scanner.nextLine(), i));
@@ -73,7 +77,7 @@ public class CityMerger {
 			out.println(item.value);
 
 			// get another item from item's source array
-			scanner = stateScanners.get(item.source);
+			scanner = scanners.get(item.source);
 
 			if (scanner.hasNextLine()) {
 				queue.insert(new PQItem(scanner.nextLine(), item.source));
@@ -83,37 +87,45 @@ public class CityMerger {
 		return;
 	} // merge(ArrayList<ArrayList<Integer>>)
 
+	/**
+	 * Merge the cities from given states into one alphabetically ascending file
+	 * 
+	 * @param args
+	 *            the names of valid states
+	 */
 	public static void main(String[] args) {
 		PrintWriter out;
 		List<Scanner> scanners = new ArrayList<>();
 		List<String> states = new ArrayList<>(args.length);
 
+		// ensure all arguments are valid
 		for (String arg : args) {
 			if (VALID_STATES.contains(arg)) {
 				states.add(arg);
 			} else {
+				// argument was not valid state
 				System.err.println("Invalid state: " + arg
 						+ ". Must choose from valid states:");
 				System.err.println(VALID_STATES);
+
 				return;
 			} // if...else
 		} // for
 
 		try {
-			
 			out = new PrintWriter(OUT_FILE);
-			scanners = getScanners(states);
-			
-			mergeCities(scanners, out);
-			
+			scanners = scannersForStates(states);
+
+			sortedFileMerge(scanners, out);
+
 			out.close();
-			
+
 		} catch (Exception e) {
-			
+			// catch any file exceptions
 			System.err.println(e);
-			
+
 		} finally {
-			
+			// always close all the scanners
 			for (Scanner scanner : scanners) {
 				scanner.close();
 			} // for
